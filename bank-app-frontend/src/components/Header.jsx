d49a563bc3fd16776d4f11fa363c7d8197034a68
YP1545 <153023750+YP1545@users.noreply.github.com>
2026-09-16 14:17:12 -0400

    Fronted working somewhat

---
+++ bank-app-frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Bank App</h1>
+      <Navbar />
+    </header>
+  );
+}
+
+export default Header;
---
+++ bank-app-frontend/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>About the Bank App</h1>
+      <p>This application is designed to manage bank accounts and transactions.</p>
+    </div>
+  );
+}
+
+export default About;
